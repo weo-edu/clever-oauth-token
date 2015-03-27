ecaec26cc160d52f36b571ba540ae3a4d6122405
Andrew Shaffer <andrew@weo.io>
2015-03-26 20:37:57 -0700

    fix some bugs

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function base64(str) {
 
 module.exports = function(clientId, clientSecret, alternateUrl) {
   var baseUrl = 'https://clever.com/oauth/tokens';
-  var authString = 'Bearer ' + base64(clientId + ':' + clientSecret);
+  var authString = 'Basic ' + base64(clientId + ':' + clientSecret);
 
   // Allow alternate base urls to facilitate
   // mock oauth endpoints
@@ -18,6 +18,10 @@ module.exports = function(clientId, clientSecret, alternateUrl) {
     // This is the only supported grant_type at the moment, so let's
     // not make people specify it if they don't want to
     params.grant_type = params.grant_type || 'authorization_code';
+    // Not well documented on Clever, at the moment, but the only value that
+    // can be passed here at the time of this writing is 'district', and if its
+    // not present, the endpoint gets upset.
+    params.owner_type = params.owner_type || 'district';
 
     var url = baseUrl + '?' + qs.stringify(params);
     request(url)
