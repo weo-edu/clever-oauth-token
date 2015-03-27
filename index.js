d743224f63c3498b85bdaf22246fcefb5c317c11
Andrew Shaffer <andrew@weo.io>
2015-03-26 20:58:36 -0700

    might actually work now

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var request = require('superagent');
-var qs = require('qs');
 
 function base64(str) {
   return (new Buffer(str)).toString('base64');
 }
 
 module.exports = function(clientId, clientSecret, alternateUrl) {
-  var baseUrl = 'https://clever.com/oauth/tokens';
+  var url = 'https://clever.com/oauth/tokens';
   var authString = 'Basic ' + base64(clientId + ':' + clientSecret);
 
   // Allow alternate base urls to facilitate
@@ -18,19 +17,15 @@ module.exports = function(clientId, clientSecret, alternateUrl) {
     // This is the only supported grant_type at the moment, so let's
     // not make people specify it if they don't want to
     params.grant_type = params.grant_type || 'authorization_code';
-    // Not well documented on Clever, at the moment, but the only value that
-    // can be passed here at the time of this writing is 'district', and if its
-    // not present, the endpoint gets upset.
-    params.owner_type = params.owner_type || 'district';
 
-    var url = baseUrl + '?' + qs.stringify(params);
-    request(url)
+    request.post(url)
       .set('Authorization', authString)
+      .send(params)
       .end(function(err, res) {
         if(err) return cb(err);
         if(res.status !== 200)
           cb(new Error(res.body));
-        cb(null, res.body.data[0].access_token);
+        cb(null, res.body.access_token);
       });
   };
 };
